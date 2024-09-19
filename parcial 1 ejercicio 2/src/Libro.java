dc68c454dbbb4cc503e099d02d97849229de9317
Florb11 <florbergman11@gmail.com>
2024-09-19 01:17:07 -0300

    imagenes

--- parcial 1 ejercicio 2/src/Libro.java
+++ parcial 1 ejercicio 2/src/Libro.java
@@ -1,25 +1,39 @@
+import javax.swing.*;
 import java.time.LocalDate;
 
 public class Libro {
     //Atributos
     private String titulo;
     private String autor;
+    private double precioAlquiler;
     private int numeroPaginas;
     private boolean estadoPrestamo;
     private LocalDate fechaPrestamo;
     private String cliente;
-    private static int numeroSerie=0;
+    private int anio;
 
-    //Constructor
+    //Constructores
 
-    public Libro(String cliente, LocalDate fechaPrestamo,
-                 boolean estadoPrestamo, int numeroPaginas, String autor, String titulo) {
-        this.cliente = cliente;
-        this.fechaPrestamo = fechaPrestamo;
-        this.estadoPrestamo = estadoPrestamo;
-        this.numeroPaginas = numeroPaginas;
-        this.autor = autor;
+    public Libro(String titulo, String autor, double precioAlquiler, int numeroPaginas, boolean estadoPrestamo, LocalDate fechaPrestamo, String cliente,int anio) {
         this.titulo = titulo;
+        this.autor = autor;
+        this.precioAlquiler = precioAlquiler;
+        this.numeroPaginas = numeroPaginas;
+        this.estadoPrestamo = estadoPrestamo;
+        this.fechaPrestamo = fechaPrestamo;
+        this.cliente = cliente;
+        this.anio=anio;
+    }
+    //hice un constructor vacio, solo inicie cliente para crearlo y usar el menu / volverme loca
+
+    public Libro(String cliente) {
+            this.titulo = "";
+            this.autor = "";
+            this.precioAlquiler = 0.0;
+            this.numeroPaginas = 0;
+            this.estadoPrestamo = false;
+            this.fechaPrestamo = LocalDate.now();
+            this.cliente = cliente;
     }
  //get y set
     public String getTitulo() {
@@ -40,8 +54,8 @@ public class Libro {
     public String getCliente() {
         return cliente;
     }
-    public static int getNumeroSerie() {
-        return numeroSerie;
+    public int getAnio() {
+        return anio;
     }
 
     public void setTitulo(String titulo) {
@@ -68,21 +82,53 @@ public class Libro {
         this.cliente = cliente;
     }
 
-    public static void setNumeroSerie(int numeroSerie) {
-        Libro.numeroSerie = numeroSerie;
+    public void setAnio(int anio) {
+        this.anio = anio;
     }
 
     //metodos
 
-    public String mostrarEstado() {
-        if (this.estadoPrestamo) {
-            return "El libro esta disponible.";
+    public void prestarLibro(String cliente) {
+        if (this.estadoPrestamo) { // si ya esta prestado
+            JOptionPane.showMessageDialog(null, "El libro no esta disponible.");
         } else {
-            return "El libro esta prestado a " + this.estadoPrestamo + " desde " + this.fechaPrestamo + ".";
+            this.estadoPrestamo = true;
+            this.cliente = cliente;
+            this.fechaPrestamo = LocalDate.now();
+            JOptionPane.showMessageDialog(null, "El libro ha sido prestado a "+ cliente +" en la fecha " + fechaPrestamo);
+        }
+
+    }
+    public void devolverLibro() {
+        if (!this.estadoPrestamo) {
+            JOptionPane.showMessageDialog(null, "El libro esta disponible.");
+        } else {
+            this.estadoPrestamo = false;
+            this.fechaPrestamo = LocalDate.now();
+            JOptionPane.showMessageDialog(null, "El libro ha sido devuelto por: " +this.cliente);
         }
     }
-    public void prestarLibros(){
-
+    public void consultarEstado() {
+        if (estadoPrestamo) {
+            JOptionPane.showMessageDialog(null, "El libro esta prestado a " + this.cliente + " desde la fecha: " + this.fechaPrestamo);
+        } else {
+            JOptionPane.showMessageDialog(null, "El libro esta disponible");
+        }
     }
 
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", precioAlquiler=" + precioAlquiler +
+                ", numeroPaginas=" + numeroPaginas +
+                ", estadoPrestamo=" + estadoPrestamo +
+                ", fechaPrestamo=" + fechaPrestamo +
+                ", cliente='" + cliente + '\'' +
+                ", anio=" + anio +
+                '}';
+    }
 }
+
--- parcial 1 ejercicio 2/src/Main.java
+++ parcial 1 ejercicio 2/src/Main.java
@@ -1,5 +1,72 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ImageIcon icon = new ImageIcon("src/img/biblio.png");
+        ImageIcon icon2 = new ImageIcon("src/img/bilbio2.png");
+
+        int opcionesMenuUno;
+
+        Libro libro1 = new Libro("It", "Stephen King", 1500.0, 1138, false, null, "",19);
+        Libro libro2 = new Libro("The Shining", "Stephen King", 12000.0,  447 , false, null, "",1);
+
+
+        do {
+            Libro libroGenerico = new Libro("");
+            String[] opciones = {"It", "The Shining", "Salir"};
+
+            opcionesMenuUno = JOptionPane.showOptionDialog(null,
+                    "Seleccione una opcion",
+                    "Menu de Libros",
+                    0,
+                    0,
+                    icon,
+                    opciones,
+                    opciones[0]);
+            switch (opcionesMenuUno) {
+                case 0:
+                    libroGenerico = libro1;
+                    break;
+                case 1:
+                    libroGenerico = libro2;
+                    break;
+                case 2:
+                    JOptionPane.showMessageDialog(null, "Final del programa");
+                    break;
+            }
+            if (opcionesMenuUno != 2) {
+                int opcionMenuLibro;
+                do {
+                    String[] opcionesLibro = {"Prestar libro", "Devolver libro", "Consultar estado", "Mostrar detalles", "Volver al menu principal"};
+                    opcionMenuLibro = JOptionPane.showOptionDialog(null,
+                            "Seleccione una opcion para el libro ",
+                            "Menu del Libro2",
+                            0,
+                            0,
+                            icon2,
+                            opcionesLibro,
+                            opcionesLibro[0]);
+
+                    switch (opcionMenuLibro) {
+                        case 0:
+                            libroGenerico.prestarLibro(JOptionPane.showInputDialog("Ingrese cliente"));
+                            break;
+                        case 1:
+                            libroGenerico.devolverLibro();
+                            break;
+                        case 2:
+                            libroGenerico.consultarEstado();
+                            break;
+                        case 3:
+                            JOptionPane.showMessageDialog(null, libroGenerico);
+                            break;
+                        case 4:
+                            JOptionPane.showMessageDialog(null, "Volviendo al menu principal");
+                            break;
+                    }
+                } while (opcionMenuLibro != 4);
+            }
+        } while (opcionesMenuUno != 2);
     }
 }
